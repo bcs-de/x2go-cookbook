8d8f53e49c017afd0bfd1dbdf2d38952959565a5
Arnold Krille <a.krille@b-c-s.de>
2013-09-05 17:00:17 +0200

    Allow xrandr-settings per client-mac
     -  Add some comments to the attributes files
     -  Allow per-client execution of xrandr-commands. Sometimes multiple displays
        in reality are not in the assumed left-to-right order. Then you need to
        execute your own xrandr commands individually for each client identified by
        mac-address.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ require 'json'
 
 default['x2go']['tce']['basedir'] = '/srv/x2gothinclient'
 
+## settings for the sessions
 default['x2go']['tce']['sessions'] = JSON.parse('''[{
 	"name": "ltsp02",
 	"command": "KDE",
@@ -16,7 +17,18 @@ default['x2go']['tce']['sessions'] = JSON.parse('''[{
 	"fullscreen": "true"
 	}]''')
 
+## Settings for calling x2goclient inside the TCE
 default['x2go']['tce']['config']['sessionedit'] = 'true'
+
+## screens to use with xinerama/xrandr for different mac-addresses
+# Screens are listet left-to-right
+default['x2go']['tce']['config']['xinerama'] = {
+  #"00:30:05:bc:3d:dd" => ['--output VGA1 --rotate left', '--output DVI1 --rotate right', '--output VGA1 --left-of DVI1']
+  "00:30:05:bc:3d:dd" => ['--output VGA1 --left-of DVI1']
+}
+## encrypted version of the root-password to use
 default['x2go']['tce']['rootpassword'] = nil
 
+## extra packages to install inside the TCE
 default['x2go']['tce']['extra_packages'] = []
+
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Arnold Krille <a.krille@b-c-s.de>'
 license          'Apache License, Version 2.0'
 description      'Installs x2go on servers and for thinclients'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports         'debian', '>= 7.0'
 
